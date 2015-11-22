b37e30d5a54dab8745eae086f5c8276f345032c7
KasparP <k.podgorski@gmail.com>
2015-11-21 21:46:25 -0500

    Python Translation: reconstruct_ADMM done

--- Python/SLAPMi/__main__.py
+++ Python/SLAPMi/__main__.py
@@ -8,94 +8,14 @@ import matplotlib.pyplot as plt
 
 fn = 'C:\Users\podgorskik\Documents\GitHub\PSI_simulations\PSI\PRECOMP_nonoise_py'
 
-
-def lossfun():
-    l = 0
-
-    # primary loss: the KL divergence between Y and Xhat
-    l_Poiss = Y[:, tidx2] * (np.log(Y[:, tidx2]) - np.log(Xhat_nn[:, tidx2]))
-    l_Poiss = sum(l_Poiss[np.isfinite(l_Poiss)]) + sum(Xhat_nn[:, tidx2] - Y[:, tidx2])
-
-    if (l_Poiss < 0) or ~np.isfinite(l_Poiss):
-        print 'LOSSFUN produced an error'
-
-    # regularizers
-    # NOTE: Do we want to sum only a subset of s? -KP
-    [u, s, v] = np.linalg.svd(Fk)
-    l_Fk_nuc = lambdaFk_nuc * sum(s)
-    [u, s, v] = np.linalg.svd(Fu)
-    l_Fu_nuc = lambdaFu_nuc * sum(s)
-
-    l = l_Poiss + l_Fk_nuc + l_Fu_nuc
-
-    print 'l_Poiss: ', l_Poiss
-    print 'l_Fk_nuc: ', l_Fk_nuc
-    print 'l_Fu_nuc: ', l_Fu_nuc
-
-    l = l / Y[:, tidx2].size
-
-    return 1
-
-
-def lossfun_aug():
-    l_aug = 0
-
-    # primary loss: the KL divergence between Y and X
-    l_Poiss = Y[:, tidx2] * (np.log(Y[:, tidx2]) - np.log(X[:, tidx2]))
-    l_Poiss = sum(l_Poiss[np.isfinite(l_Poiss)]) + sum(X[:, tidx2] - Y[:, tidx2])
-    l_X = (rho / 2) * sum((Xhat[:, tidx2] - X[:, tidx2] + np.square(U_X[:, tidx2]) - np.square(U_X[:, tidx2])))
-
-    l_Fk = 0
-    [u, s, v] = np.linalg.svd(Fk_nuc)
-    l_Fk = l_Fk + lambdaFk_nuc * sum(s)
-    l_Fk = l_Fk + (rho / 2) * sum(
-        (np.flatten(Fk_nuc) - np.flatten(Fk) + np.square(np.flatten(U_Fk_nuc)) - np.square(np.flatten(U_Fk_nuc))))
-    l_Fk = l_Fk + (rho / 2) * sum(
-        (np.flatten(Fk_nn) - np.flatten(Fk) + np.square(np.flatten(U_Fk_nn)) - np.square(np.flatten(U_Fk_nn))))
-    # l_Fk = l_Fk + lambdaFk_TF*sum(Fk_TF*)
-    # l_Fk = l_Fk + lambdaFk_TF*sum(Fk_TF*)
-
-    l_Fu = 0
-    [u, s, v] = np.linalg.svd(Fu_nuc)
-    l_Fu = l_Fu + lambdaFu_nuc * sum(s)
-    l_Fu = l_Fu + (rho / 2) * sum(
-        (np.flatten(Fu_nuc) - np.flatten(Fu) + np.square(np.flatten(U_Fu_nuc)) - np.square(np.flatten(U_Fu_nuc))))
-    l_Fu = l_Fu + (rho / 2) * sum(
-        (np.flatten(Fu_nn) - np.flatten(Fu) + np.square(np.flatten(U_Fu_nn)) - np.square(np.flatten(U_Fu_nn))))
-
-    l_Sk = 0
-    l_Sk = l_Sk + (rho / 2) * sum(
-        (np.flatten(Sk_nn) - np.flatten(Sk) + np.square(np.flatten(U_Sk_nn)) - np.square(np.flatten(U_Sk_nn))))
-    l_Su = 0
-    l_Su = l_Su + (rho / 2) * sum(
-        (np.flatten(Su_nn) - np.flatten(Su) + np.square(np.flatten(U_Su_nn)) - np.square(np.flatten(U_Su_nn))))
-
-    l_aug = l_Poiss + l_X + l_Fk + l_Fu + l_Sk + l_Su
-
-    print 'l_Poiss: ', l_Poiss
-    print 'l_X: ', l_X
-    print 'l_Fk: ', l_Fk
-    print 'l_Fu: ', l_Fu
-    print 'l_Sk: ', l_Sk
-    print 'l_Su: ', l_Su
-
-    l_aug = l_aug / Y[:, tidx2].size
-    return l_aug
-
-
-def lossfun_gt():  # WE NEED A PROPER GT LOSS FUNCTION!
-    l_gt = np.linalg.norm(Fk[:, tidx] - Fk0[:, tidx], 'fro') + np.linalg.norm(np.flatten(Sk) - np.flatten(Sk0))
-    return l_gt
-
-
 # def initialize(fn):
 D = io.loadmat(fn, struct_as_record=False, squeeze_me=True)
 # print type(D['ground_truth']['seg'])
 # reconstruct_imaging_ADMM(D['obs'],D['opts'],1000,40,D['ground_truth'])
 obs = D['obs']
 opts = D['opts']
-nIter = 1000
-Nframes = 40
+nIter = 3
+Nframes = 2
 groundtruth = D['ground_truth']
 
 # NOTES:
@@ -171,7 +91,6 @@ def Pu(it):
 def Pk(it):
     return P0[:, mask]
 
-
 # IS THIS THE CORRECT SIZE TO INITIALIZE THESE?
 PSFu = sp.zeros(Y.shape)
 PSFk = sp.zeros(Y.shape)
@@ -181,21 +100,98 @@ loss = sp.zeros((1, nIter))
 loss_aug = sp.zeros((1, nIter))
 loss_gt = sp.zeros((1, nIter))
 
+if Nframes < T:
+    tidx = random.sample(range(0, T), Nframes)
+else:
+    tidx = range(0, T)  # 1:T
+tidx2 = range(0, T)
+
 print 'Done initialization!', time.time() - tic, 'seconds'
 
-if Nframes < T:
-    tidx = random.sample(range(1, T + 1), Nframes)
-else:
-    tidx = range(1, T + 1)  # 1:T
+#Function definitions
+def lossfun():
+    l = 0
 
-tidx2 = range(1, T + 1)
+    # primary loss: the KL divergence between Y and Xhat
+    l_Poiss = Y[:, tidx2] * (np.log(Y[:, tidx2]) - np.log(Xhat_nn[:, tidx2]))
+    l_Poiss = sum(l_Poiss[np.isfinite(l_Poiss)]) + sum(sum(Xhat_nn[:, tidx2] - Y[:, tidx2]))
+
+    if (l_Poiss < 0) or ~np.isfinite(l_Poiss):
+        print 'LOSSFUN produced an error'
+
+    # regularizers
+    # NOTE: Do we want to sum only a subset of s? -KP
+    [u, s, v] = np.linalg.svd(Fk)
+    l_Fk_nuc = lambdaFk_nuc * sum(s)
+    [u, s, v] = np.linalg.svd(Fu)
+    l_Fu_nuc = lambdaFu_nuc * sum(s)
+
+    l = l_Poiss + l_Fk_nuc + l_Fu_nuc
+
+    print 'l_Poiss: ', l_Poiss
+    print 'l_Fk_nuc: ', l_Fk_nuc
+    print 'l_Fu_nuc: ', l_Fu_nuc
+
+    l = l / Y[:, tidx2].size
+
+    return 1
+
+def lossfun_aug():
+    l_aug = 0
+
+    # primary loss: the KL divergence between Y and X
+    l_Poiss = Y[:, tidx2] * (np.log(Y[:, tidx2]) - np.log(X[:, tidx2]))
+    l_Poiss = sum(l_Poiss[np.isfinite(l_Poiss)]) + sum(sum(X[:, tidx2] - Y[:, tidx2]))
+    l_X = (rho / 2) * sum(sum((Xhat[:, tidx2] - X[:, tidx2] + np.square(U_X[:, tidx2]) - np.square(U_X[:, tidx2]))))
+
+    l_Fk = 0
+    [u, s, v] = np.linalg.svd(Fk_nuc)
+    l_Fk = l_Fk + lambdaFk_nuc * sum(s)
+    l_Fk = l_Fk + (rho / 2) * sum(
+        (ndarray.flatten(Fk_nuc) - ndarray.flatten(Fk) + np.square(ndarray.flatten(U_Fk_nuc)) - np.square(ndarray.flatten(U_Fk_nuc))))
+    l_Fk = l_Fk + (rho / 2) * sum(
+        (ndarray.flatten(Fk_nn) - ndarray.flatten(Fk) + np.square(ndarray.flatten(U_Fk_nn)) - np.square(ndarray.flatten(U_Fk_nn))))
+    # l_Fk = l_Fk + lambdaFk_TF*sum(Fk_TF*)
+    # l_Fk = l_Fk + lambdaFk_TF*sum(Fk_TF*)
+
+    l_Fu = 0
+    [u, s, v] = np.linalg.svd(Fu_nuc)
+    l_Fu = l_Fu + lambdaFu_nuc * sum(s)
+    l_Fu = l_Fu + (rho / 2) * sum(
+        (ndarray.flatten(Fu_nuc) - ndarray.flatten(Fu) + np.square(ndarray.flatten(U_Fu_nuc)) - np.square(ndarray.flatten(U_Fu_nuc))))
+    l_Fu = l_Fu + (rho / 2) * sum(
+        (ndarray.flatten(Fu_nn) - ndarray.flatten(Fu) + np.square(ndarray.flatten(U_Fu_nn)) - np.square(ndarray.flatten(U_Fu_nn))))
+
+    l_Sk = 0
+    l_Sk = l_Sk + (rho / 2) * sum(
+        (ndarray.flatten(Sk_nn) - ndarray.flatten(Sk) + np.square(ndarray.flatten(U_Sk_nn)) - np.square(ndarray.flatten(U_Sk_nn))))
+    l_Su = 0
+    l_Su = l_Su + (rho / 2) * sum(
+        (ndarray.flatten(Su_nn) - ndarray.flatten(Su) + np.square(ndarray.flatten(U_Su_nn)) - np.square(ndarray.flatten(U_Su_nn))))
+
+    l_aug = l_Poiss + l_X + l_Fk + l_Fu + l_Sk + l_Su
+
+    print 'l_Poiss: ', l_Poiss
+    print 'l_X: ', l_X
+    print 'l_Fk: ', l_Fk
+    print 'l_Fu: ', l_Fu
+    print 'l_Sk: ', l_Sk
+    print 'l_Su: ', l_Su
+
+    l_aug = l_aug / Y[:, tidx2].size
+    return l_aug
+
+def lossfun_gt():  # WE NEED A PROPER GT LOSS FUNCTION! Previous one was sensitive to swapping of Sk's
+    l_gt = 10
+    return l_gt
+
 for ITER in range(1, nIter + 1):
     # subsample in time
     if Nframes < T:
-        tidx = random.sample(range(1, T + 1), Nframes)
+        tidx = random.sample(range(0, T), Nframes)
         tidx2 = tidx
         if ITER == 1 or (ITER % 50) == 0:
-            tidx2 = range(1, T + 1)
+            tidx2 = range(0, T)
     print ('Pre-computing P*[Sk*Fk+Su*Fu]... ')
     tic = time.time()
     # b=0;
@@ -203,6 +199,7 @@ for ITER in range(1, nIter + 1):
 
 
     for it in tidx2:
+        print it, '\r'
         PSFu[:, it] = Pu(it).dot(Su.dot(Fu[:, it]))
         PSFk[:, it] = Pk(it).dot(Sk.dot(Fk[:, it]))
         Xhat_nn[:, it] = Pu(it).dot(np.maximum(Su, 0)).dot(np.maximum(Fu[:, it], 0)) + Pk(it).dot((np.maximum(Sk, 0)).dot(np.maximum(Fk[:, it], 0)))
@@ -210,8 +207,7 @@ for ITER in range(1, nIter + 1):
 
     Xhat[:, tidx2] = PSFu[:, tidx2] + PSFk[:, tidx2]  # no divide by 2?
     print 'Done precompute. ', time.time() - tic, 'seconds'
-
-    X[:, tidx2] = prox_DKL(Y[:, tidx2], Xhat[:, tidx2] - U_X[:, tidx2], rho)
+#   X[:, tidx2] = prox_DKL(Y[:, tidx2], Xhat[:, tidx2] - U_X[:, tidx2], rho)
     U_X_PSFk[:, tidx2] = U_X[:, tidx2] + X[:, tidx2] - PSFk[:, tidx2]
     U_X_PSFu[:, tidx2] = U_X[:, tidx2] + X[:, tidx2] - PSFu[:, tidx2]
 
@@ -242,21 +238,21 @@ for ITER in range(1, nIter + 1):
         Fk[:, it] = sp.linalg.solve(PSk.T.dot(PSk) + 2 * sp.eye(Nsk), (
         PSk.T.dot(U_X_PSFu[:, it]) + (U_Fk_nuc[:, it] + U_Fk_nuc[:, it]) + (Fk_nuc[:, it] + Fk_nn[:, it])))
         print '\b' * 8
-        print "'%08d'" % it
+        print "'%08d'\r" % it
 
     print '. Done. '
     print 'Estimating S... \n'
 
     Cu = U_Su_nn + Su_nn
-    tidx = range(1, T + 1)  # randsample(T,10);
+    tidx = range(0, T)  # randsample(T,10);
     for it in tidx:  # 1:length(Tidx),
-        Cu = Cu + Pu(it).T.dot(U_X_PSFk[:, it].dot(Fu[:, it].T))
-    Su = solve_S_Pfun(Pu, Fu, Cu, Su, tidx)
+        Cu = Cu + Pu(it).T.dot(np.expand_dims(U_X_PSFk[:, it],1).dot(np.expand_dims(Fu[:, it],1).T))
+#    Su = solve_S_Pfun(Pu, Fu, Cu, Su, tidx)
 
     Ck = U_Sk_nn + Sk_nn
     for it in tidx:  # 1:length(tidx),
-        Ck = Ck + Pk(it).T.dot(U_X_PSFu[:, it].dot(Fk[:, it].T))
-    Sk = solve_S_Pfun(Pk, Fk, Ck, Sk, tidx)
+        Ck = Ck + Pk(it).T.dot(np.expand_dims(U_X_PSFu[:, it],1).dot(np.expand_dims(Fk[:, it],1).T))
+#    Sk = solve_S_Pfun(Pk, Fk, Ck, Sk, tidx)
 
     print 'Done. '
 
@@ -276,22 +272,22 @@ for ITER in range(1, nIter + 1):
     U_Sk_nn = U_Sk_nn + Sk_nn - Sk
 
     ## Compute loss
-    loss[ITER] = lossfun()
-    loss_aug[ITER] = lossfun_aug()
-    if 'groundtruth' in locals() and groundtruth.size > 0:
-        loss_gt[ITER] = lossfun_gt()
+    loss[0,ITER] = lossfun()
+    loss_aug[0,ITER] = lossfun_aug()
+    if 'groundtruth' in locals() and groundtruth is not None:
+        loss_gt[0,ITER] = lossfun_gt()
     else:
-        loss_gt[ITER] = 0
+        loss_gt[0,ITER] = 0
 
     # print '[Iter: %d] Loss: %f, Loss aug: %f, Loss gt: %f\n\n',iter,loss(iter),loss_aug(iter),loss_gt(iter))
 
     tvec = range(2, ITER + 1)
     tvec2 = np.arange(51, ITER, 50).tolist()
     plt.subplot(211)
-    plt.plot(tvec, loss(tvec), tvec2, loss(tvec2))
+    plt.plot(tvec, loss[tvec], tvec2, loss[tvec2])
     plt.title('loss')
     plt.subplot(212)
-    plt.plot(tvec, loss_aug(tvec), tvec2, loss_aug(tvec2))
+    plt.plot(tvec, loss_aug[tvec], tvec2, loss_aug[tvec2])
     plt.title('augmented loss')
     # plt.subplot(223)
     # imagesc(Sk'*Sk0)
