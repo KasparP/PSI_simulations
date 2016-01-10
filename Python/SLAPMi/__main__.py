bc791953d3f63d369e805fb191cc7abf1d7d167b
Srini Turaga <turagas@janelia.hhmi.org>
2016-01-10 17:40:53 -0500

    dropped the learning rate a bit

--- Python/SLAPMi/__main__.py
+++ Python/SLAPMi/__main__.py
@@ -297,7 +297,7 @@ if __name__ == '__main__':
 
     print 'Done initialization!', time.time() - tic, 'seconds'
 
-    Sk,Fk,Su,Fu = reconstruct_cpu(Y,Sk,Fk,Su,Fu,Nframes,nIter,eta=1e-1,adagrad=False)
+    Sk,Fk,Su,Fu = reconstruct_cpu(Y,Sk,Fk,Su,Fu,Nframes,nIter,eta=1e-2,adagrad=False)
     # Sk,Fk,Su,Fu = reconstruct_theano(Y,Sk,Fk,Su,Fu,Nframes,nIter)
 
     # Fu_nuc = Fu  # zeros(size(Fu));
