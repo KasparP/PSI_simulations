38f373e3d1c0d533398ed0079930af6be7215371
KasparP <k.podgorski@gmail.com>
2016-01-13 14:33:08 -0500

    move from dm11 to tier2

--- Python/expt1/runexpt.py
+++ Python/expt1/runexpt.py
@@ -1,7 +1,7 @@
 import os, sys
-sys.path.append('/groups/turaga/turagalab/SLAPmi/Python')
+sys.path.append('tier2/turaga/podgorskik/PSI_simulations/Python')
 import SLAPMi
 
 (Y,Sk,Fk,Su,Fu, GT) = SLAPMi.prepexpt()
-Sk,Fk,Su,Fu = SLAPMi.reconstruct_cpu(Y[:,:25],Sk,Fk[:,:25],Su,Fu[:,:25],Nframes=200,nIter=int(1e5),eta=1e1,mu=0.7,adagrad=True, groundtruth=GT, out_fn='/groups/turaga/turagalab/SLAPmi/Python/expt1/output.mat')
+Sk,Fk,Su,Fu = SLAPMi.reconstruct_cpu(Y[:,:25],Sk,Fk[:,:25],Su,Fu[:,:25],Nframes=200,nIter=int(1e5),eta=1e1,mu=0.7,adagrad=True, groundtruth=GT, out_fn='tier2/turaga/podgorskik/PSI_simulations/Python/expt1/output.mat')
 
