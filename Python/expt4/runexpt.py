e5743cdb42ed2a37c30baa182019661d318af327
KasparP <k.podgorski@gmail.com>
2016-02-10 14:30:27 -0500

    Experiment scripts

--- Python/expt4/runexpt.py
+++ Python/expt4/runexpt.py
@@ -5,6 +5,6 @@ sys.path.append('/tier2/turaga/podgorskik/PSI_simulations/Python')
 
 import SLAPMi
 
-(Y,Sk,Fk,Su,Fu, GT) = SLAPMi.prepexpt('../Problem_nonoise_1Kframes_init.mat')
+(Y,Sk,Fk,Su,Fu, GT) = SLAPMi.prepexpt('../Problem_noise_20Kframes_init.mat')
 Sk,Fk,Su,Fu = SLAPMi.reconstruct_cpu(Y,Sk,Fk,Su,Fu,Nframes=2,nIter=int(1e5),eta=1e-6,mu=0.9,adagrad=True, groundtruth=GT)
 
